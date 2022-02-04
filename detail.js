1f0a4617531c713d509efc679d144b776a0500c2
ferru03 <76011910+ferru03@users.noreply.github.com>
2022-02-04 11:08:05 +0100

    nuovo

--- detail.js
+++ detail.js
@@ -16,10 +16,15 @@ const app = Vue.createApp({
     },
 
     methods:{
-      
+        convertDate(inputFormat) {
+            function pad(s) { return (s < 10) ? '0' + s : s; }
+            var d = new Date(inputFormat)
+            return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('/')
+        }
     },
     computed:{    
     
     },
 
   })
+  
